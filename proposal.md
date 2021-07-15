d16ac180ca150d232441f48e6b3fc69f990ae75d
teknofage <teknofage@gmail.com>
2021-07-15 01:54:14 -0700

    updated proposal and added basic go scraper

--- proposal.md
+++ proposal.md
@@ -2,18 +2,19 @@
 
 What do you wish was easier?
 
-
+*Knowing whether an article was worth reading, and whether the amount of attention it gets is proportionate to the amount of comments it gets.*
 
 What utility, API, or library could have the most impact on your day to day life?
 
-
+*Being able to scrap metadata about articles and filter them using that data. For example, on the ycombinator news site.*
 
 What can I automate that would make myself and others more productive?
 
-
+*I could automate a process which reads the number of Points each Article has, and compares it to the number of Comments that same Article has.*
 
 How could you make your colleagues' day more fun, interesting, or relevant?
 
+*I could share this information with them on an hourly basis, so they could go leave a comment in the most (or least, depending on preference) popular and trending articles.*
 
 When can the unique features of Golang be applied in order to produce a polished product quickly?
 
---
+++ scrape.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	// "io/ioutil"
+	"os"
+	"io"
+
+
+	"github.com/gocolly/colly"
+)
+
+
+type article struct {
+	Title 		string	`json:"title"`
+	URL 		string	`json:"URL"`
+	Score 		string 	`json:"score"`
+	Comments 	string 	`json:"score"`
+	Poster 		string	`json:"poster"`
+}
+
+func main() {
+	var articles []article
+	var pageCount int
+	i := 0
+	
+	// Instantiate default collector
+	c := colly.NewCollector()
+	cssSelector := "tbody > tr:nth-child(3) > td > table > tbody"
+	
+
+
+	c.OnHTML(cssSelector, func(e *colly.HTMLElement) {
+				e.ForEach("tr", func(_ int, h *colly.HTMLElement) {
+					var art article
+					title := h.ChildText("td.title > a") 
+					score := h.ChildText("td.subtext > span.score")
+					comments := h.ChildText("td.subtext > a:last-child")
+					if title == "More" {
+						c.Visit("news.ycombinator.com/" + h.ChildAttr("td.title > a", "href"))
+					} else if score != "" {
+						articles[i].Score = score
+						articles[i].Comments = h.ChildText("td.subtext > a:last-child")
+						articles[i].Poster = h.ChildText("td.subtext > a.hnuser")
+						i++
+					} else if title != "" {
+						
+						art.Title = title
+						art.URL = h.ChildAttr("td.title > a", "href")
+						articles = append(articles, art)
+					}
+					
+				})
+
+	})
+	// Before making a request print "Visiting ..."
+	c.OnResponse(func(r *colly.Response) {
+		pageCount++
+        urlVisited := r.Request.URL
+        fmt.Println(fmt.Sprintf("%d  DONE Visiting : %s", pageCount, urlVisited))
+	})
+
+	// Start scraping on https://news.ycombinator.com
+	c.Visit("https://news.ycombinator.com/")
+	fmt.Println(articles[0])
+	fmt.Println("We did it!")
+
+	// Marshal instances of articles and conert to JSON
+	articleJSON, _ := json.Marshal(articles)
+	// fmt.Println(string(articleJSON))
+	articleJSONString := string(articleJSON)
+	// fmt.Println(articleJSONString)
+	writeJSONToFile(articleJSONString)
+}
+
+
+
+func writeJSONToFile(articleJSONString string) error {
+    file, err := os.Create("output.json")
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    _, err = io.WriteString(file, articleJSONString)
+    if err != nil {
+        return err
+    }
+    return file.Sync()
+}
